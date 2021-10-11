bc3cbdf1555753ff11c7d1356687d265486ca33b
mooye <lidy@skyvis.com.cn>
2021-10-11 15:31:31 +0800

    Initial commit

---
+++ src/main/java/com/mooye/navicatim/NavicatImApplication.java
@@ -0,0 +1,15 @@
+package com.mooye.navicatim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication
+public class NavicatImApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NavicatImApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/mooye/navicatim/NavicatImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooye.navicatim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavicatImApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
